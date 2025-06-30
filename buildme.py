2e52d90b04a9654b7dfce84b008499e7fac41301
katusek1235 <szymek2017r@gmail.com>
2025-06-30 13:25:10 +0200

    Start

---
+++ buildme.py
@@ -0,0 +1,15 @@
+create_vscode_settings = True
+run_after_build        = True
+proj_name              = "bvlc"
+proj_version           = "0.1.0.0"
+file_name              = "bvlc.exe"
+compiler               = "g++"
+linker                 = "g++"
+flags                  = ['-Wall','-Wextra','-O3']
+compiler_flags         = ""
+linker_flags           = ""
+output_path            = "bin"
+source_path            = "src"
+libs                   = []
+lib_paths              = []
+include_paths          = []
